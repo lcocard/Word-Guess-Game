3fc0d9e62e4738de67bfa2e461d680d7f22ce58e
Lawrence Cocard <12580746+lcocard@users.noreply.github.com>
2019-06-08 22:56:12 -0400

    aded alerts for winning or losing the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -73,6 +73,7 @@ var currentGuessWordText = document.getElementById("currentGuessWord-text");
 var guessesremainingText = document.getElementById("guessesremaining-text");
 var userGuessLetterText = document.getElementById("userGuessLetter-text");
 var songText = document.getElementById("song-text");
+
 function appendImage(imageSource, containerId, imageId) {
     var img = document.createElement("IMG");
     img.src = imageSource;
@@ -89,27 +90,29 @@ function removeImage(imageId) {
 
 winsText.textContent = "Wins: " + wins;
 songText.textContent = " ";
-/* Build array to be dispalyed for Current Word */
+// Build array to be dispalyed for Current Word
 for (var s = 1; s < splitWordGuessPick.length; s++) {
     currentGuessWord.push("- ");
 }
 currentGuessWordText.textContent = " Current Word: " + currentGuessWord.join('');
 guessesremainingText.textContent = " Number of guesses remaining: " + guessesremaining;
 userGuessLetterText.textContent = " Letters already guessed: " + userGuessLetter;
+
+alert("Guess the name of a band from the 80's!")
+
 // This function is run whenever the user presses a key.
 document.onkeyup = function (event) {
     var alreadyGuessedLetterIndex = 0;
     var foundLetterFlag = 0;
     var wrongLetterIndex = 0;
-    // This loop is for limiting the user tries to 20 and increasing the wining number if the entire word was guessed
+
+    // This loop is for limiting the user tries to 20 and increasing the wining number if the entire word was guessed:
     for (var i = 0; i < 20; i++) {
-        // This loop is for checking if there are any letters remaining to be guessed. It will terminate the loop that iterates 20 times, as the word has been guessed already, and we will reset the variables
-        // The image will be changed with a new one showing the band, there will be a title displayed at the top and a song will play
 
         // Determines which key was pressed.
         var userGuessLetterElement = event.key;
         console.log(userGuessLetterElement);
-        // This logic determines if the key pressed by the user is one of the letters from the word to be guessed
+        // This logic determines if the key pressed by the user is one of the letters from the word to be guessed:
         for (var k = 0; k < splitWordGuessPick.length; k++) {
             if (userGuessLetterElement === splitWordGuessPick[k]) {
                 currentGuessWord[k] = userGuessLetterElement;
@@ -122,17 +125,19 @@ document.onkeyup = function (event) {
                 if (alreadyGuessedLetterIndex === 1) {
                     userGuessLetter.push(userGuessLetterElement);
                     userGuessLetterText.textContent = " Letters already guessed: " + userGuessLetter.join('');
+                    // Checking if the user has guessed the entire word:
                     if (JSON.stringify(currentGuessWord) == JSON.stringify(splitWordGuessPick)) {
                         wins++;
                         winsText.textContent = " Wins: " + wins;
                         songText.textContent = " " + songTitleWordGuessPick;
                         removeImage("pic-text");
                         var myImage = appendImage(picWordGuessPick, "band-pic", "pic-text");
+                        alert("Well done!");
                     }
                 }
             }
             else {
-                // Hide the directions
+                // Hide the directions:
                 directionsText.textContent = "";
                 wrongLetterIndex++;
             }
@@ -140,8 +145,9 @@ document.onkeyup = function (event) {
         if (foundLetterFlag !== 1 && wrongLetterIndex === splitWordGuessPick.length) {
             guessesremaining = guessesremaining - 1;
             guessesremainingText.textContent = " Number of guesses remaining: " + guessesremaining;
+            if (guessesremaining === 0) {
+                alert("Sorry, zero guesses remaining, please try again!");
+            }
         }
-
-
     }
 }
