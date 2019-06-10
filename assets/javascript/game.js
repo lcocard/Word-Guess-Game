dbe275cd8f47d11774703c128e4230e8dbd65d4b
Lawrence Cocard <12580746+lcocard@users.noreply.github.com>
2019-06-10 02:49:31 -0400

    added a restart button and removed j iterations

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,81 +1,103 @@
+// Word-Guess-Game - game_init.js
+
 // debugger
-var alreadyGuessedLetterIndex = 0;
-var foundLetterFlag = 0;
-var wrongLetterIndex = 0;
-var wins = 0;
-var wordGuessArray = [
-    {
-        name: "genesis",
-        songTitle: "Genesis",
-        pic: "assets/images/genesis.png"
-    },
-    {
-        name: "madonna",
-        songTitle: "Madonna",
-        pic: "assets/images/MADONNA.png"
-    },
-    {
-        name: "blondie",
-        songTitle: "Blondie",
-        pic: "assets/images/blondie.png"
-    },
-    {
-        name: "queen",
-        songTitle: "Queen",
-        pic: "assets/images/queen-band.png"
-    },
-    {
-        name: "rush",
-        songTitle: "Rush",
-        pic: "assets/images/Rush-band.png"
-    },
-    {
-        name: "police",
-        songTitle: "Police",
-        pic: "assets/images/thepolice.png"
-    },
-    {
-        name: "metallica",
-        songTitle: "Metallica",
-        pic: "assets/images/metallica.png"
-    },
-    {
-        name: "aerosmith",
-        songTitle: "Aerosmith",
-        pic: "assets/images/aerosmith.png"
-    },
-    {
-        name: "scorpions",
-        songTitle: "Scorpions",
-        pic: "assets/images/scorpions.png"
-    },
-    {
-        name: "eurythmics",
-        songTitle: "Eurythmics",
-        pic: "assets/images/eurythmics.png"
-    }
-]
-/* ***** Pick random word from array  */
-var wordGuessPick = wordGuessArray[Math.floor(Math.random() * wordGuessArray.length)];
-/*  ***** Split word into array */
-var splitWordGuessPick = wordGuessPick.name.split("");
-var songTitleWordGuessPick = wordGuessPick.songTitle;
-var picWordGuessPick = wordGuessPick.pic;
-console.log(splitWordGuessPick);
-console.log(wordGuessPick.songTitle);
-console.log(wordGuessPick.pic);
-var currentGuessWord = ["- "];
 var guessesremaining = 20;
-var userGuessLetter = [" "];
 var directionsText = document.getElementById("directions-text");
 var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
 var currentGuessWordText = document.getElementById("currentGuessWord-text");
 var guessesremainingText = document.getElementById("guessesremaining-text");
 var userGuessLetterText = document.getElementById("userGuessLetter-text");
 var songText = document.getElementById("song-text");
+var wins = 0;
+var losses = 0;
+var reset_flag = false;
+function initialize() {
+    removeImage("pic-text");
+    appendImage("assets/images/4-WordGuess.jpg", "band-pic", "pic-text");
+    var alreadyGuessedLetterIndex = 0;
+    var foundLetterFlag = 0;
+    var wrongLetterIndex = 0;
+    var wordGuessArray = [
+        {
+            name: "madonna",
+            songTitle: "Madonna",
+            pic: "assets/images/MADONNA.png"
+        },
+        {
+            name: "genesis",
+            songTitle: "Genesis",
+            pic: "assets/images/genesis.png"
+        },
+        {
+            name: "blondie",
+            songTitle: "Blondie",
+            pic: "assets/images/blondie.png"
+        },
+        {
+            name: "queen",
+            songTitle: "Queen",
+            pic: "assets/images/queen-band.png"
+        },
+        {
+            name: "rush",
+            songTitle: "Rush",
+            pic: "assets/images/Rush-band.png"
+        },
+        {
+            name: "police",
+            songTitle: "Police",
+            pic: "assets/images/thepolice.png"
+        },
+        {
+            name: "metallica",
+            songTitle: "Metallica",
+            pic: "assets/images/metallica.png"
+        },
+        {
+            name: "aerosmith",
+            songTitle: "Aerosmith",
+            pic: "assets/images/aerosmith.png"
+        },
+        {
+            name: "scorpions",
+            songTitle: "Scorpions",
+            pic: "assets/images/scorpions.png"
+        },
+        {
+            name: "eurythmics",
+            songTitle: "Eurythmics",
+            pic: "assets/images/eurythmics.png"
+        }
+    ]
+    /* ***** Pick random word from array  */
+    wordGuessPick = wordGuessArray[Math.floor(Math.random() * wordGuessArray.length)];
+    /*  ***** Split word into array */
+    splitWordGuessPick = wordGuessPick.name.split("");
+    songTitleWordGuessPick = wordGuessPick.songTitle;
+    picWordGuessPick = wordGuessPick.pic;
+    console.log(splitWordGuessPick);
+    console.log(wordGuessPick.songTitle);
+    console.log(wordGuessPick.pic);
+    currentGuessWord = ["- "];
+    guessesremaining = 20;
+    userGuessLetter = [" "];
+
+
+    winsText.textContent = "Wins: " + wins;
+    lossesText.textContent = "Losses: " + losses;
+    songText.textContent = " ";
+    // Build array to be dispalyed for Current Word
+    for (var s = 1; s < splitWordGuessPick.length; s++) {
+        currentGuessWord.push("- ");
+    }
+    currentGuessWordText.textContent = " Current Word: " + currentGuessWord.join('');
+    guessesremainingText.textContent = " Number of guesses remaining: " + guessesremaining;
+    userGuessLetterText.textContent = " Letters already guessed: " + userGuessLetter;
+}
 
 function appendImage(imageSource, containerId, imageId) {
-    var img = document.createElement("IMG");
+    img = document.createElement("IMG");
     img.src = imageSource;
     img.setAttribute('id', imageId);
     document.getElementById(containerId).appendChild(img);
@@ -83,31 +105,26 @@ function appendImage(imageSource, containerId, imageId) {
 }
 
 function removeImage(imageId) {
-    var elementToBeRemoved = document.getElementById(imageId);
+    elementToBeRemoved = document.getElementById(imageId);
     elementToBeRemoved.parentNode.removeChild(elementToBeRemoved);
 }
 
 
-winsText.textContent = "Wins: " + wins;
-songText.textContent = " ";
-// Build array to be dispalyed for Current Word
-for (var s = 1; s < splitWordGuessPick.length; s++) {
-    currentGuessWord.push("- ");
-}
-currentGuessWordText.textContent = " Current Word: " + currentGuessWord.join('');
-guessesremainingText.textContent = " Number of guesses remaining: " + guessesremaining;
-userGuessLetterText.textContent = " Letters already guessed: " + userGuessLetter;
+console.log(guessesremaining);
 
-alert("Guess the name of a band from the 80's!")
+initialize();
+
+// alert("Guess the name of a band from the 80's!");
 
 // This function is run whenever the user presses a key.
+
 document.onkeyup = function (event) {
     var alreadyGuessedLetterIndex = 0;
     var foundLetterFlag = 0;
     var wrongLetterIndex = 0;
 
     // This loop is for limiting the user tries to 20 and increasing the wining number if the entire word was guessed:
-    for (var i = 0; i < 20; i++) {
+    // for (var i = 0; i < 20; i++) {
 
         // Determines which key was pressed.
         var userGuessLetterElement = event.key;
@@ -126,13 +143,27 @@ document.onkeyup = function (event) {
                     userGuessLetter.push(userGuessLetterElement);
                     userGuessLetterText.textContent = " Letters already guessed: " + userGuessLetter.join('');
                     // Checking if the user has guessed the entire word:
-                    if (JSON.stringify(currentGuessWord) == JSON.stringify(splitWordGuessPick)) {
+                    if (JSON.stringify(currentGuessWord) === JSON.stringify(splitWordGuessPick)) {
                         wins++;
                         winsText.textContent = " Wins: " + wins;
                         songText.textContent = " " + songTitleWordGuessPick;
                         removeImage("pic-text");
-                        var myImage = appendImage(picWordGuessPick, "band-pic", "pic-text");
-                        alert("Well done!");
+                        myImage = appendImage(picWordGuessPick, "band-pic", "pic-text");
+                        console.log(guessesremaining);
+                        console.log(JSON.stringify(currentGuessWord));
+                        console.log(JSON.stringify(splitWordGuessPick));
+
+
+                        // document.body.addEventListener("click", initialize(),{once:true})
+                        if (window.alert("Well done, you won!")) {
+                            location.reload();
+                            document.onkeyup = function (event) {
+                                wins = wins;
+                                losses = losses;
+
+
+                            }
+                        }
                     }
                 }
             }
@@ -146,8 +177,14 @@ document.onkeyup = function (event) {
             guessesremaining = guessesremaining - 1;
             guessesremainingText.textContent = " Number of guesses remaining: " + guessesremaining;
             if (guessesremaining === 0) {
-                alert("Sorry, zero guesses remaining, please try again!");
+                losses++;
+                lossesText.textContent = "Losses: " + losses;
+                console.log(guessesremaining);
+                if (window.alert("Sorry, you lost")) {
+                    location.reload();
+                    document.onkeyup = function (event) {}
+                }
             }
         }
-    }
+    // }
 }
